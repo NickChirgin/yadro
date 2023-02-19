e47044d6f020ade1bc0acf93e7f7bc785ca6ade8
Nikolay <nickchirgin@gmail.com>
2023-02-19 15:51:37 +0300

    added error handling for non-existing rows and columns

--- helpers/transform.go
+++ helpers/transform.go
@@ -1,6 +1,8 @@
 package helpers
 
 import (
+	"fmt"
+	"log"
 	"regexp"
 	"strings"
 )
@@ -20,6 +22,12 @@ func StringToCell(csv [][]string, cell string) ([]string, string) {
 			if i == 1 {
 				col = split[i][1:]
 				math = split[i][:1]
+			}
+			if _, ok := colMap[col]; !ok {
+				log.Fatal(fmt.Errorf("Column %s doesn't exist", col))
+			}
+			if _, ok := rowMap[val[0]]; !ok{
+				log.Fatal(fmt.Errorf("Row %s doesn't exist", val[0]))
 			}
 			numSlice = append(numSlice, csv[rowMap[val[0]]][colMap[col]])
 		}
